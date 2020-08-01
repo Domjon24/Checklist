b9039e2b7ab208c341e64a9671447fead5056a3d
Dominique Jones <Domjon24@gmail.com>
2020-08-01 09:35:03 -0500

    first draft

--- scripts.js
+++ scripts.js
@@ -1,13 +1,44 @@
-// Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+function validateForm() {
+   var x = document.forms["myForm"]["fname"].value;
+   if (x == "") {
+     alert("Name must be filled out");
+     return false;
+   }
+   if (typeof inputs[0].value !== "string" || typeof inputs[1].value !== "string") {
+    alert('Please enter a valid name!')
+    e.preventDefault()
+}
+
+if (isNaN(Number(inputs[2].value)) || isNaN(Number(inputs[3].value))) {
+    alert('Please enter a valid number!')
+    e.preventDefault()
+}
+ }
+const formValid = document.getElementById('myForm');
+formValid.addEventListener('click', function (e) {
+else.preventDefault();
+});
+
+function showMission() {
+    fetch('https://handlers.education.launchcode.org/static/planets.json').then((response) => {
+      response.json().then((json : Planet[]) => {
+        const destination = document.getElementById('missionTarget')
+
+            if (destination) {
+                let formTemplate = `<h2>Mission Destination</h2>
+                <ol>
+                   <li>Name: ${}</li>
+                   <li>Diameter: ${}</li>
+                   <li>Star: ${}</li>
+                   <li>Distance from Earth: ${}</li>
+                   <li>Number of Moons: ${}</li>
+                </ol>
+                <img src="${}">`
+                destination.innerHTML = formTemplate
+            } else {
+                console.log('document elements not found!')
+            }
+        })
+    })
+}
